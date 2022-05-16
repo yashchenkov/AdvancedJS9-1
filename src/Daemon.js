a0e06469ed606abd804edf661854acec0374f9c7
Yashchenko Victor <Andrey@Komp11.local>
2022-05-16 23:05:26 +0300

    fixing

--- src/Daemon.js
+++ src/Daemon.js
@@ -1,31 +1,7 @@
-export default class Daemon {
+import Character from './Character';
+
+export default class Daemon extends Character{
   constructor(attack) {
-  	this.attack = attack;
-  	this.realAttack = attack;
-  	this.stoned = 0;
-  }
-
-  getAttack() {
-  	return this.realAttack;
-  }
-
-  setAttack(distance) {
-    this.setStoned(distance);
-    this.realAttack = this.attack;
-    if(distance >= 10) {
-      this.realAttack = 0;
-    } else if(distance === 0) {
-      throw new Error('Нельзя атаковать самого себя');
-    } else {
-      this.realAttack = this.attack*(1 - (distance - 1)*0.1) - this.getStoned(); 
-    }
-  }
-
-  getStoned() {
-  	return this.stoned;
-  }
-  setStoned(distance) {
-  	this.stoned = 0;
-  	this.stoned = Math.log2(distance) * 5;
+  	super(attack);
   }
 }
