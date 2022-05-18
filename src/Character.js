f27990afa71e13c0eb61b0531646728637022ba3
Yashchenko Victor <Andrey@Komp11.local>
2022-05-18 20:24:36 +0300

    final fix

--- src/Character.js
+++ src/Character.js
@@ -11,25 +11,25 @@ export default class Character {
         throw new Error('Нельзя атаковать самого себя');
       }
 
-      let realAttack = this.attack*(1 - (this.distance - 1)*0.1);;
-      return this.hasOwnProperty('stoned') && this.stoned ? realAttack - Math.log2(this.distance) * 5 : realAttack;
+      let realAttack = this.characterAttack * (1 - (this.distance - 1)*0.1);;
+      return this.hasOwnProperty('characterStoned') && this.characterStoned ? realAttack - Math.log2(this.distance) * 5 : realAttack;
     } 
-    return this.attack;
+    return this.characterAttack;
   }
 
   set attack(attack) {
-    this.attack = attack;
+    this.characterAttack = attack;
   }
 
   get stoned() {
-    if(this.hasOwnProperty('stoned')){
-      return this.stoned;
+    if(this.hasOwnProperty('characterStoned')){
+      return this.characterStoned;
     }
   }
 
   set stoned(stoned) {
-    if(this.hasOwnProperty('stoned')) {
-      this.stoned = stoned;
+    if(this.hasOwnProperty('characterStoned')) {
+      this.characterStoned = stoned;
     }
   }
 }
--- src/Daemon.js
+++ src/Daemon.js
@@ -3,7 +3,7 @@ import Character from './Character';
 export default class Daemon extends Character{
   constructor() {
     super();
-    this.stoned = false;
+    this.characterStoned = false;
     this.distance = 2;
   }
 }
